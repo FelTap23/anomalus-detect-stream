4488201d324785a27b1c93c2be13d434d021df95
FelTap23 <felipeTapia1223@gmail.com>
2019-10-15 21:51:34 -0600

    First Commit

---
+++ src/main/java/com/anomalous/detection/AnomalousDetectionStreamApplication.java
@@ -0,0 +1,13 @@
+package com.anomalous.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalousDetectionStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnomalousDetectionStreamApplication.class, args);
+	}
+
+}
